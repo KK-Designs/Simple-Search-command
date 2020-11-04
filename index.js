10bd6556f18560e8a98a2a0e0799b723c691ab7f
KK-Designs <71038229+KK-Designs@users.noreply.github.com>
2020-11-04 09:21:24 -0800

    Added a search command

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+  client.user.setActivity(` !help | Serving ${client.guilds.cache.size} servers`, { type: 'LISTENING' });
+});
+
+client.on("message", async message => {
+
+       if (message.author.bot) return;
+
+	   const args = message.content.slice(prefix.length).split(' ');
+     const command = args.shift().toLowerCase();
+
+	if (message.content === '!ping') {
+		message.channel.send('Pong.');
+	}
+  
+  if(command === 'searchweb') {
+  /*if there are no arguments provided*/ 
+    if (!args.length) {
+      return message.reply('Please enter a search (ie: `!searchweb how to make pancakes`)');
+    }
+   /*if there is a space in search add a '+' to the URL*/ 
+    const webargs = message.content.slice(10).trim().split(' ').join('+');
+    const url = `https://www.google.com/search?q=${webargs}`
+    message.channel.send(`Here is your web search result: \n \n ${url}`)
+  }
+
+  if(command === 'searchyt') {
+  /*if there are no arguments provided*/ 
+    if (!args.length) {
+      return message.reply('Please enter a search (ie: `!searchyt poppinpoptarts`)');
+    }
+    /*if there is a space in search add a '+' to the URL*/ 
+    const ytargs = message.content.slice(9).trim().split(' ').join('+');
+    const url = `https://www.youtube.com/results?search_query=${ytargs}`
+    message.channel.send(`Here is your youtube search result: \n \n ${url}`)
+  }
+  
+});
+
+/*Made By K.K Designs 🙂*/
+client.login('your-token-goes-here');
