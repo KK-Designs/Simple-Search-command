312d8b5b23d0109d43f5bce26e2a05bc16adaa23
KK-Designs <71038229+KK-Designs@users.noreply.github.com>
2020-11-04 09:38:19 -0800

    Added a search command

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
   client.user.setActivity(` !help | Serving ${client.guilds.cache.size} servers`, { type: 'LISTENING' });
 });
 
 client.on("message", async message => {
 
-       if (message.author.bot) return;
+      /*Ignores messages from other bots*/ 
+        if (message.author.bot) return;
 
-	   const args = message.content.slice(prefix.length).split(' ');
+     const args = message.content.slice(prefix.length).split(' ');
      const command = args.shift().toLowerCase();
 
 	if (message.content === '!ping') {
-		message.channel.send('Pong.');
+	    message.channel.send('Pong.');
 	}
   
   if(command === 'searchweb') {
